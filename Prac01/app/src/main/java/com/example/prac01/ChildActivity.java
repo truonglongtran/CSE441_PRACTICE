1be6a64f00d0a28f23fdb149b5317d6b7a6fde54
truonglongtran <longtran2308bn@gmail.com>
2024-09-28 15:01:44 +0700

    add prac01

---
+++ Prac01/app/src/main/java/com/example/prac01/ChildActivity.java
@@ -0,0 +1,39 @@
+package com.example.prac01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_child);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnSubmit = findViewById(R.id.btn_submit);
+        EditText edtName = findViewById(R.id.edt_name);
+        EditText edtGPA = findViewById(R.id.edt_GPA);
+        btnSubmit.setOnClickListener(v -> {
+            String name = edtName.getText().toString();
+            String GPA = edtGPA.getText().toString();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("GPA", GPA);
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+    }
+}
---
+++ Prac01/app/src/main/java/com/example/prac01/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.prac01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+    private TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        txtResult = findViewById(R.id.txt_result);
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ChildActivity.class);
+            startActivityForResult(intent,REQUEST_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE&&resultCode == RESULT_OK&&data!=null){
+            String name = data.getStringExtra("name");
+            String GPA = data.getStringExtra("GPA");
+            txtResult.setText("Name: " + name + "\nGPA: " + GPA);
+        }
+    }
+}
